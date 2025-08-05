3c4066c5a9bb1200f4417c2e1a391800e79f8837
kannan <kannans89@hotmail.com>
2025-08-05 17:53:58 +0530

    correct portno

--- index.js
+++ index.js
@@ -1,13 +1,9 @@
 const express = require('express');
 const app = express();
-const port = 3000;
-
-// Simple route that returns "Hello World"
+const port = process.env.PORT || 3000
 app.get('/', (req, res) => {
-  res.send('Hello World! to Wipro Folks');
+res.send('<h1>Hello, this is my web application! Wipro  folks</h1>');
 });
-
-// Start the server
 app.listen(port, () => {
-  console.log(`Server is running on http://localhost:${port}`);
+console.log(`Server is running at http://localhost:${port}`);
 });
